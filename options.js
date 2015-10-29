bf3dfc103cbc22e0af9f4f77ebdfd98b827a7763
Gustav Nikolaj Olsen <gno@one.com>
2015-10-29 15:01:03 +0100

    update package metadata

--- options.js
+++ options.js
@@ -4,8 +4,8 @@ var packageJson = require('./package.json');
 module.exports = {
     cmd: 'onelint',
     version: packageJson.version,
-    //homepage: packageJson.homepage,
-    //bugs: packageJson.bugs.url,
+    homepage: packageJson.homepage,
+    bugs: packageJson.bugs.url,
     tagline: 'One JavaScript Style to Rule them All!',
     eslintConfig: {
         configFile: path.join(__dirname, 'eslintrc.json')
